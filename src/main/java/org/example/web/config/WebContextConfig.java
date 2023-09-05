d025ec3758f89b2da7cee9e5a01703f30bb5b1b2
lebedev.sv.kst <lebede.beard@yandex.ru>
2023-09-05 09:56:04 +0300

    glossaries done 2

--- src/main/java/org/example/web/config/WebContextConfig.java
+++ src/main/java/org/example/web/config/WebContextConfig.java
@@ -11,18 +11,16 @@ import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
-@ComponentScan(basePackages = "org.example") //context:component-scan base-package="org.example.web"
-@EnableWebMvc //<mvc:annotation-driven/>
+@ComponentScan(basePackages = "org.example")
 public class WebContextConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/images");
-        //<mvc:resources mapping="/**" location="classpath:images"/>
     }
 
     @Bean
-    public SpringResourceTemplateResolver templateResolver() {  //   <bean id="templateResolver"
+    public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
@@ -34,7 +32,7 @@ public class WebContextConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public SpringTemplateEngine templateEngine() { // <bean id="templateEngine"
+    public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
@@ -44,7 +42,7 @@ public class WebContextConfig implements WebMvcConfigurer {
 
 
     @Bean
-    public ThymeleafViewResolver viewResolver() { // <bean class="org.thymeleaf.spring6.view.ThymeleafViewResolver">
+    public ThymeleafViewResolver viewResolver() {
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
         viewResolver.setTemplateEngine(templateEngine());
         viewResolver.setOrder(1);
@@ -52,6 +50,4 @@ public class WebContextConfig implements WebMvcConfigurer {
 
         return viewResolver;
     }
-
-
 }
--- src/main/java/org/example/web/controllers/ErrorController.java
+++ src/main/java/org/example/web/controllers/ErrorController.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @ControllerAdvice
-public class ErrorController {
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
-    @GetMapping("/404")
+    @RequestMapping("/error")
     public String notFoundError(Model model) {
         model.addAttribute("errorMassage", "404 Not Found");
         model.addAttribute("urlToBack", "/glossaries/shelf");
